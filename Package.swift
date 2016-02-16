ae2f79989e0bdfa75a2d6edd2da866d2945d2a73
Paulo Faria <paulo.faria.rl@gmail.com>
2016-02-16 09:42:51 -0200

    add missing packages required because of annoying SPM bug

--- Package.swift
+++ Package.swift
@@ -5,6 +5,8 @@ let package = Package(
 	dependencies: [
 		.Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 2),
 		.Package(url: "https://github.com/Zewo/Base64.git", majorVersion: 0, minor: 2),
-		.Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 2)
+		.Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/Zewo/CHTTPParser.git", majorVersion: 0, minor: 2)
 	]
 )
