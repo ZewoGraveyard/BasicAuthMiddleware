cf110d168aa3088d3b57459d47c4363c711260ff
Paulo Faria <paulo.faria.rl@gmail.com>
2016-02-16 09:52:26 -0200

    fix tabs vs spaces

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
 import PackageDescription
 
 let package = Package(
-	name: "BasicAuthMiddleware",
-	dependencies: [
-		.Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 2),
-		.Package(url: "https://github.com/Zewo/Base64.git", majorVersion: 0, minor: 2),
-		.Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 2),
+    name: "BasicAuthMiddleware",
+    dependencies: [
+        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/Zewo/Base64.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/Zewo/CHTTPParser.git", majorVersion: 0, minor: 2)
-	]
+    ]
 )
