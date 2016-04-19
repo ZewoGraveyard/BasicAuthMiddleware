351382f8dff7ecee167641eecdf88633c747b642
Paulo Faria <paulo.faria.rl@gmail.com>
2016-04-19 17:50:01 -0300

    bump to 0.5.0

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "BasicAuthMiddleware",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/Zewo/Base64.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/Zewo/Base64.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 5),
     ]
 )
