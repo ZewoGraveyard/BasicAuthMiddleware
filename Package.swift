0dc7f34daa169e242d044c5532231fc10ce89da5
Anton <antonmes@users.noreply.github.com>
2016-05-13 01:53:58 +0400

    Update Base64 dependency to 0.7

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "BasicAuthMiddleware",
     dependencies: [
         .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 5),
-        .Package(url: "https://github.com/Zewo/Base64.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/Zewo/Base64.git", majorVersion: 0, minor: 7),
         .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 5),
     ]
 )
